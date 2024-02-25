f194038056f50f0d8776fb65ae719d6647db7a67
Robert Faber <robertsfaber@gmail.com>
2024-02-24 22:03:47 -0500

    Added functionality for game logic. Working on board next.

--- Game_Main.cs
+++ Game_Main.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Threading;
 
+// https://c-sharpcorner.com/UploadFile/75a48f/tic-tac-toe-game-in-C-Sharp/
+
 namespace TicTacToe
+
 {
 	class Program
 	{
@@ -17,27 +20,62 @@ namespace TicTacToe
 			{
 				Console.Clear();
 				Console.WriteLine("Player 1: X and Player 2: O\n");
-				
+
 				if (player % 2 == 0)
 				{
 					Console.WriteLine("Player 2 Chance");
 				}
-                else
-                {
+				else
+				{
 					{
 						Console.WriteLine("Player 1 Chance");
 					}
-                }
+				}
 				Console.WriteLine("\n");
 
 				Board();
-				choice = int.Parse(Console.ReadLine());
+				choice = int.Parse(Console.ReadLine()); // takes user choice input
+				if (arr[choice] != 'X' && arr[choice] != '0')
+				{
+					if (player % 2 != 0)
+					{
+						arr[choice] = '0';
+						player++;
 
-                // pin here
-                // https://c-sharpcorner.com/UploadFile/75a48f/tic-tac-toe-game-in-C-Sharp/
+					}
+					else
+					{
+						arr[choice] = 'X';
+						player++;
+					}
+				}
+				else
+				{
+					Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
+					Console.WriteLine("\n");
+					Console.WriteLine("Please wait while board loads...");
+					Thread.Sleep(2000);
+				}
+				flag = CheckWin()
+			}
+			while (flag != 1 && flag != -1);
+			Console.Clear();
+			Board();
+			if (flag == 1)
+			{
+				Console.WriteLine("Player {0} has won", (player % 2) + 1);
+			}
+            else
+            {
+				Console.WriteLine("Draw");
             }
-
+			Console.ReadLine()
         }
+
+		private static void Board()
+		{
+			// pin
+		}
 
 	}
 }
